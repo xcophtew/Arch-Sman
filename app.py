ef2a4f084957efbdf74b01ab88cc2dcdf3aa33a7
Grey <bloodecho001@gmail.com>
2024-08-22 11:11:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template as rt
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return rt('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
